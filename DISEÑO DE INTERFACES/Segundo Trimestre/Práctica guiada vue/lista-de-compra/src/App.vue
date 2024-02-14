de4682d058399bcd52a71945c7c95dc6ad8e33f2
CarlosAlvarez96 <calvarezmartin96@gmail.com>
2024-02-14 14:42:18 +0100

    empezada practica guiada vue

---
+++ DISEÑO DE INTERFACES/Segundo Trimestre/Práctica guiada vue/lista-de-compra/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {ref} from 'vue';
+import EncabezadoComponente from './components/EncabezadoComponente.vue'
+import PieComponente from './components/PieComponente.vue'
+import ListaItems from './components/ListaItems.vue'
+import NuevoItem from './components/NuevoItem.vue'
+
+let listado = ref([
+  {
+    productos:"Pan",
+    prioridad:"baja"
+  },
+  {
+    productos:"Leche",
+    prioridad:"media"
+  },
+  {
+    productos:"Manzanas",
+    prioridad:"alta"
+  }
+])
+</script>
+
+
+
+<template>
+  <EncabezadoComponente/>
+  <ListaItems :misCompras='listado'/>
+  <NuevoItem/>
+  <PieComponente/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ DISEÑO DE INTERFACES/Segundo Trimestre/Práctica guiada vue/lista-de-compra/src/components/NuevoItem.vue
@@ -0,0 +1,45 @@
+<script setup>
+    import {ref} from 'vue';
+    const emit = defineEmits(['agregarItem'])
+
+    const item = ref('');
+    const priority=ref('');
+    const handleClick = () => {
+        const nuevoProducto={
+            producto:item.value,
+            prioridad:priority.value
+        }
+        emit('agregarItem', nuevoProducto);
+        item.value = '';
+        priority.value='';
+    }
+</script>
+
+<template>
+    <section>
+        <h4>Nuevo Item</h4>
+        <input type=text placeholder=Agua vmodel=Item autofocus>
+        <select name=priority v-model="priority">
+            <option value="" disabled>prioridad</option>
+            <option value="baja" disabled>🟢 - baja</option>
+            <option value="media" disabled>🟡 - media</option>
+            <option value="alta" disabled>🔴 - alta</option>
+        </select>
+    </section>
+    <button v-show="item && priority">
+        <font-awesome-icon icon="fa-plus"/>
+    </button>
+</template>
+
+<style scoped>
+    input,select{
+        font-family: 'Genos', sans-serif;
+        font-family: 'Gloria Hallelujah', cursive;
+        background-color:black;
+        border-radius:4px;
+        font-size:1.4rem;
+        color:white;
+        width:40%;
+        margin-right:10px;
+    }
+</style>
